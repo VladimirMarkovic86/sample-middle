17d16e0332e50b338fcb11745718b897baaf2ccb
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-09-25 16:16:44 +0200

    Code formatting.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://gitlab:1610/VladimirMarkovic86/sample-middle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure	"1.9.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
                  [org.vladimir/common-middle "0.1.0"]
                  ]
