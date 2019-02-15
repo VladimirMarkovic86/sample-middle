a750834f6289d72771fdffc54814625da0516774
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-15 11:03:04 +0100

    Task 54: Fixed po and ocr

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/sample-middle "0.3.4"
+(defproject org.clojars.vladimirmarkovic86/sample-middle "0.3.5"
   :description "Sample middleware"
   :url "http://github.com/VladimirMarkovic86/sample-middle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.4"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
                  ]
 
   :min-lein-version "2.0.0"
