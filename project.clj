245ad3235cd95967511064eeb25d949c4ad47c2a
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-06-14 14:28:38 +0200

    Task 64: Implemented card view in html and latex reports, preferences window is available also.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/sample-middle "0.3.9"
+(defproject org.clojars.vladimirmarkovic86/sample-middle "0.3.10"
   :description "Sample middleware"
   :url "http://github.com/VladimirMarkovic86/sample-middle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.7"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.8"]
                  ]
 
   :min-lein-version "2.0.0"
--- src/cljc/sample_middle/person/entity.cljc
+++ src/cljc/sample_middle/person/entity.cljc
@@ -27,6 +27,24 @@
 (def activity-very-hard-physical-labor
      "very_hard_physical_labor")
 
-(def rows
-     10)
+(def table-rows-a
+     (atom 10))
+
+(def card-columns-a
+     (atom 0))
+
+(def preferences
+     {:table-rows-a table-rows-a
+      :card-columns-a card-columns-a})
+
+(defn calculate-rows
+  "Calculates rows for table/card view"
+  []
+  (if (= (int
+           @card-columns-a)
+         0)
+    @table-rows-a
+    (* @table-rows-a
+       @card-columns-a))
+ )
 
