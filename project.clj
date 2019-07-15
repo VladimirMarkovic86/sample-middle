2844590ab44c06f186e07a01a712fa3b246eabee
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:53:58 +0200

    Task 88: Removed README.md and LICENSE files from packed jar files.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/sample-middle "0.3.11"
+(defproject org.clojars.vladimirmarkovic86/sample-middle "0.3.12"
   :description "Sample middleware"
   :url "http://github.com/VladimirMarkovic86/sample-middle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.10"]
                  ]
 
   :min-lein-version "2.0.0"
