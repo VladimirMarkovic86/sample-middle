9914ef632f6e7b7bb12fbdee58f3385d3f4592a1
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 18:01:23 +0200

    Task 88: Rolled back README.md and LICENSE files to appear in jar files.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/sample-middle "0.3.12"
+(defproject org.clojars.vladimirmarkovic86/sample-middle "0.3.13"
   :description "Sample middleware"
   :url "http://github.com/VladimirMarkovic86/sample-middle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.10"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.11"]
                  ]
 
   :min-lein-version "2.0.0"
